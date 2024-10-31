36d890b0ea3f4b07fdfb92cc560046224704287a
joaopedrorori <joaopedrorr.df@gmail.com>
2024-10-31 10:57:11 -0300

    lista de animais funcionando da maneira correta, ao clicar na foto de tal animal, aparecera a descricao do mesmo. Caso o usuario tenha a opcao de desabilitar javascript ativada no navegador, o site continuara funcionando normalmente como na versao anterior

---
+++ script.js
@@ -0,0 +1,16 @@
+const tabMenu = document.querySelectorAll(".js-tabmenu li");
+const tabContent = document.querySelectorAll(".js-tabcontent section");
+
+if (tabMenu.length && tabContent.length) {
+  tabContent[0].classList.add("ativo");
+  console.log(tabMenu, tabContent);
+
+  function activeTab(index) {
+    tabContent.forEach((item) => item.classList.remove("ativo"));
+    tabContent[index].classList.add("ativo");
+  }
+
+  tabMenu.forEach((img, i) => {
+    img.addEventListener("click", () => activeTab(i));
+  });
+}
