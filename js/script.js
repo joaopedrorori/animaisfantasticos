52f329c5e0d11bd79fb19617b6f79788eb7f58e2
joaopedrorori <joaopedrorr.df@gmail.com>
2025-01-06 18:03:55 -0300

    webpack adicionado, e as dependencias jquery e lodash foram adicionadas

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
-// import initFuncionamento from "./modules/funcionamento.js";
+import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 
@@ -18,6 +18,6 @@ initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
-// initFuncionamento();
+initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
